a0293a2ed6de2e592b9dcb596768fb394a0e00c4
Harsh Gupta <80761963+HarshGuptaJonty@users.noreply.github.com>
2021-10-08 11:33:46 +0530

    Add files via upload
    updated loader of all screens

--- JS/Setup2.js
+++ JS/Setup2.js
@@ -1,15 +1,9 @@
-// var myVar;
-
-// function myFunction() {
-//     myVar = setTimeout(showPage, 3000);
-// }
-
 function showPage() {
-    document.getElementById("loader").style.display = "none";
+    document.getElementById("load").style.display = "none";
     document.getElementById("fader").style.display = "none";
 }
 
 function hidepage(){
-    document.getElementById("loader").style.display = "block";
+    document.getElementById("load").style.display = "block";
     document.getElementById("fader").style.display = "block";
 }
