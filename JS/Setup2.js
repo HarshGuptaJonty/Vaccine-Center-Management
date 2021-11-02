0c1a65ad3307ec1d2187eb94fdcb6fc4037fd603
Harsh Gupta <80761963+HarshGuptaJonty@users.noreply.github.com>
2021-11-02 22:36:03 +0530

    Update Setup2.js
    added goTop function

--- JS/Setup2.js
+++ JS/Setup2.js
@@ -7,3 +7,8 @@ function hidepage(){
     document.getElementById("load").style.display = "block";
     document.getElementById("fader").style.display = "block";
 }
+
+function goTop(){
+    document.body.scrollTop = 0; //for safari
+    document.documentElement.scrollTop = 0;
+}
